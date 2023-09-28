ddd5133137f3556bd862ab16c79c0b01c1e4712e
TTG <6306021621065@fitm.kmutnb.ac.th>
2023-09-28 23:17:33 +0700

    Initial commit

---
+++ sever/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+
+app.use(cors());
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        medthods: ["GET", "POST"],
+    },
+});
+
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+
+    socket.on("join_room", (data) =>{
+        socket.join(data);
+        console.log(`User With ID: ${socket.id} join room: ${data}`)
+    });
+
+    socket.on("sent_message", (data) =>{
+        socket.to(data.room).emit("receive_message", data);
+    });
+    
+    socket.on("disconnect", () => {
+        console.log("User Disconnect", socket.id);
+    });
+});
+
+server.listen(3001, () => {
+    console.log("SERVER RUNNING!!!");
+});
